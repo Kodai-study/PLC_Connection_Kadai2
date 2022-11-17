bddb415501571113106d259aa8a6532889d1ee65
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-11-17 10:09:23 +0900

    Initial commit

---
+++ PLC_Connection/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MITSUBISHI.Component;
+
+
+namespace PLC_Connection
+{
+   class Class1
+    {
+        public static void Main()
+        {
+            DotUtlType dotUtlType = new DotUtlType() { ActLogicalStationNumber = 1 };
+            dotUtlType.Open();
+        }
+    }
+}
