5d24ab9551a8ba90a4c7035ddf76bd13f04c7dab
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-11-21 10:56:33 +0900

    クラスを分ける、入力があるとその時間を入力

--- PLC_Connection/Class1.cs
+++ PLC_Connection/Class1.cs
@@ -2,58 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MITSUBISHI.Component;
-
+using System.Data.SqlClient;
+using ActUtlTypeLib;
 
 namespace PLC_Connection
 {
     class Class1
     {
+        public DotUtlType dotUtlType;
         public static void Main()
         {
-            DotUtlType dotUtlType = new DotUtlType();
-            dotUtlType.ActLogicalStationNumber = 1;
-            int open = dotUtlType.Open();
+            var logicCtrl = new LogicCtrl();
+            int errCode = 0;
 
 
-            string rabel = "テスト";
-            var buff = new int[2];
-            int read = dotUtlType.ReadDeviceBlock(ref rabel,2, ref buff);
-            Console.WriteLine(buff);
-
-            rabel = "Test5";
-            int device = 5;
-            read = dotUtlType.GetDevice(ref rabel, ref device);
-
-            rabel = "OutTest";
-            read = dotUtlType.GetDevice(ref rabel, ref device);
-           // Console.WriteLine(device);
-
-            rabel = "テスティング";
-            device = 0;
-            int count = 0;
-            while (device != 0 || count < 1000)
-            {
-                read = dotUtlType.GetDevice(ref rabel, ref device);
-                if (read != 0)
-                {
-                    Console.WriteLine("エラー");
-                    break;
-                }
-                else if (device != 0)
-                {
-                    Console.WriteLine("受け取りします");
-                    break;
-                }
-                System.Threading.Thread.Sleep(100);
-                count++;
-            }
-            
+            if (logicCtrl.waitTrigger("テスティング", ref errCode))
+                ConnectDB();
         }
 
 
+        static void ConnectDB()
+        {
+            SqlConnection a = new SqlConnection("Data Source=RBPC12;Initial Catalog=Robot22_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            a.Open();
+
+            string cmd = String.Format("INSERT INTO PLC_Test (Time) VALUES ({0})", DateTime.Now);
+            using (var command = new SqlCommand(cmd, a))
+                command.ExecuteNonQuery();
+        }
     }
 }
---
+++ PLC_Connection/LogicCtrl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MITSUBISHI.Component;
+using System.Data.SqlClient;
+using ActUtlTypeLib;
+
+namespace PLC_Connection
+{
+    class LogicCtrl
+    {
+        private DotUtlType dotUtlType;
+        public void a()
+        {
+            dotUtlType = new DotUtlType();
+            dotUtlType.ActLogicalStationNumber = 1;
+            int open = dotUtlType.Open();
+
+
+            string rabel = "テスト";
+            var buff = new int[2];
+            int read = dotUtlType.ReadDeviceBlock(ref rabel, 2, ref buff);
+            Console.WriteLine(buff);
+
+            rabel = "Test5";
+            int device = 5;
+            read = dotUtlType.GetDevice(ref rabel, ref device);
+
+            rabel = "OutTest";
+            read = dotUtlType.GetDevice(ref rabel, ref device);
+            // Console.WriteLine(device);
+
+        }
+
+
+        public bool waitTrigger(string rabel,ref int code)
+        {
+            int device = 0;
+            int count = 0;
+            int Code = 0;
+            while (device != 0 || count < 1000)
+            {
+                Code = dotUtlType.GetDevice(ref rabel, ref device);
+                if (Code != 0)
+                {
+                    Console.WriteLine("エラー");
+                    code = Code;
+                    return false;
+                }
+                else if (device == 1)
+                {
+                    return true;
+                }
+                Thread.Sleep(100);
+                count++;
+            }
+            return false;
+        }
+    }
+}
