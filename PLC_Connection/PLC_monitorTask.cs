a18cfee3e365d3104af3ef0f4c0c8a11ea3d0ba2
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-12-05 16:39:29 +0900

    サイクルタイムを順番に入れていくように実装

--- PLC_Connection/PLC_monitorTask.cs
+++ PLC_Connection/PLC_monitorTask.cs
@@ -11,6 +11,7 @@ using System.Data.SqlClient;
 using ActUtlTypeLib;
 using System.Data;
 using System.Reflection.Emit;
+using System.Collections;
 
 namespace PLC_Connection
 {
@@ -18,14 +19,22 @@ namespace PLC_Connection
     {
         public DotUtlType dotUtlType;
 
+        /// <summary>
+        /// 　PLCの読み取りタスクを終了させる命令を出すトークン
+        /// </summary>
         private CancellationTokenSource cancellToken;
 
+        /// <summary>
+        /// データベースへの書込みを行うコネクションオブジェクト
+        /// </summary>
         SqlConnection sqlConnection;
 
-        bool b = false;
+        WorkController workController;
+
         public async Task<bool> Start()
         {
             this.cancellToken = new CancellationTokenSource();
+            workController = new WorkController();
             dotUtlType = new DotUtlType();
             dotUtlType.ActLogicalStationNumber = 401;
             if (dotUtlType.Open() != 0)
@@ -46,18 +55,18 @@ namespace PLC_Connection
             int read = dotUtlType.ReadDeviceBlock(ref label, 1, ref datas);
             int old_data = datas[0];
             Task<bool> dbWriteTask = null;
+
+
             while (true)
             {
                 read = dotUtlType.ReadDeviceBlock(ref label, 1, ref datas);
-
-
                 if (old_data != datas[0])
                 {
                     DateTime dateTime = DateTime.Now;
                     int diff = datas[0] ^ old_data;
-                    dbWriteTask = Task.Run(() => Task_WriteDB(dateTime,diff,datas[0]));
+                    old_data= datas[0];
+                    dbWriteTask = Task.Run(() => writedata(dateTime, diff, datas[0]));
                     dbWrite = true;
-                    old_data = datas[0];
                 }
 
 
@@ -67,7 +76,7 @@ namespace PLC_Connection
                     Console.WriteLine("MyThread IsCancellationRequested");
                     break;
                 }
-                Thread.Sleep(10);
+                Thread.Sleep(2);
 
                 if (dbWrite)
                 {
@@ -77,17 +86,66 @@ namespace PLC_Connection
             }
         }
 
-        public bool Task_WriteDB(DateTime nowTime,int changeBit,int sensorData)
+        /// <summary>
+        ///  サンプルで、触れたセンサ値(Xﾗﾍﾞﾙ)の種類と、
+        ///  触れたか離れたかの値をデータベースに入力
+        /// </summary>
+        /// <param name="nowTime">  </param>
+        /// <param name="changeBit"></param>
+        /// <param name="sensorData"></param>
+        /// <returns></returns>
+        public bool Task_WriteDB(DateTime nowTime, int changeBit, int sensorData)
         {
             try
             {
                 int sensorNum = 0;
                 int on_off = (changeBit & sensorData) != 0 ? 1 : 0;
 
-                for(;changeBit != 0;changeBit >>= 1,sensorNum++) {; }
+                for (; changeBit != 0; changeBit >>= 1, sensorNum++) {; }
 
-                string cmd = String.Format("INSERT INTO PLC_Test (Time,sensor_Number,ON_OFF) VALUES ('{0}.{1:D3}' , {2} , {3})", nowTime, nowTime.Millisecond,sensorNum, on_off);
+                string cmd = String.Format("INSERT INTO PLC_Test (Time,sensor_Number,ON_OFF) VALUES ('{0}.{1:D3}' , {2} , {3})", nowTime, nowTime.Millisecond, sensorNum, on_off);
                 using (var command = new SqlCommand(cmd, sqlConnection))
+                    command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 　変化したPLCの値から、検査工程が進んでいた時はデータベースを更新する
+        /// </summary>
+        /// <param name="reactTime"> 変化を検知した時刻 </param>
+        /// <param name="changeBit"> 変化していたビット </param>
+        /// <param name="sensorData"> 変化していた値の元の値 </param>
+        /// <returns></returns>
+        public bool writedata(DateTime reactTime, int changeBit, int sensorData)
+        {
+
+            bool ChangeON = (changeBit & sensorData) != 0;  //変化したビットが、1に変化したか(true)
+            string sql = null;
+            int progressNum = -1;       //行われた変化
+
+            try
+            {
+                /* 変化が工程の進みと関係なかったら何もしない */
+                if (!Parameters.Bit_X.getProgressNum(changeBit, ChangeON, ref progressNum))
+                {
+                    return false;
+                }
+
+                if (progressNum == 0)
+                {
+                    sql = workController.AddnewWork(reactTime);
+                }
+                else if (progressNum > 0)
+                {
+                    sql = workController.ProcessToSql(progressNum, reactTime.TimeOfDay);
+                }
+                using (var command = new SqlCommand(sql, sqlConnection))
                     command.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -97,19 +155,96 @@ namespace PLC_Connection
             return true;
         }
 
-
-
         public void hoge()
         {
             string label = "wordSample";
             int data = 0;
             dotUtlType.GetDevice(ref label, ref data);
-
         }
 
+        /// <summary>
+        ///  PLC状態取得のポーリングを停止≒プログラムの終了
+        /// </summary>
+
+        ///  TODO 検査(のデータ取得)の終了ログの追加
         public void stop()
         {
             cancellToken.Cancel();
         }
+    }
+
+    /// <summary>
+    ///  検査ステーションに入ってきたワークの管理を行う
+    ///  
+    /// </summary>
+    class WorkController
+    {
+        /// <summary>
+        ///  ステーション内にあるワークの一覧を管理
+        ///  (検査終了、搬出でキューから無くなる)
+        /// </summary>
+        private Queue<WorkData> t = null;
+        public WorkController()
+        {
+            t = new Queue<WorkData>();
+        }
+
+        public string ProcessToSql(int progressNum, TimeSpan nowTime)
+        {
+            string sql = null;
+            if (progressNum == 9)
+            {
+                sql = String.Format("UPDATE Test_CycleTime SET {0} = '{1}' WHERE Carry_in = {2}",
+                    "Carry_out", nowTime, t.Dequeue().startTime);
+            }
+            else
+            {
+                foreach (var e in t)
+                {
+                    if (progressNum > e.progressNum)
+                    {
+                        e.progressNum = progressNum;
+                        sql = String.Format("UPDATE Test_CycleTime SET {0} = '{1}' WHERE Carry_in = {2}",
+                        Parameters.TIME_COLUMNAMES[progressNum], nowTime, t.Dequeue().startTime);
+                    }
+                }
+            }
+            return sql;
+        }
+
+        /// <summary>
+        /// 　新しいワークが搬入された時に管理対象に追加する
+        /// </summary>
+        /// <param name="startTime">　搬入された時刻　</param>
+        public string AddnewWork(DateTime startTime)
+        {
+            t.Enqueue(new WorkData(startTime, 1));
+            return String.Format("INSERT INTO Test_CycleTime ({2}) VALUES ('{0}.{1:D3}')",
+                startTime, startTime.Millisecond, Parameters.TIME_COLUMNAMES[0]);
+        }
+
+        /// <summary>
+        ///  検査データを表すクラス
+        /// </summary>
+        class WorkData
+        {
+            public int progressNum;
+            public DateTime startTime;
+            public WorkData(DateTime startTime, int progressNum)
+            {
+                this.progressNum = progressNum;
+                this.startTime = startTime;
+            }
+        }
+    }
+}
+
+class WorkStatus
+{
+    public int status;
+    Queue<int> workQueue;
+    public WorkStatus()
+    {
+        workQueue = new Queue<int>();
     }
 }
---
+++ PLC_Connection/Parameters.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MITSUBISHI.Component;
+using System.Data.SqlClient;
+using ActUtlTypeLib;
+using System.Reflection.Emit;
+
+namespace PLC_Connection
+{
+    public static class Parameters
+    {
+        public static readonly string[] TIME_COLUMNAMES =
+            { "Carry_in", "Position", "Shoot_S", "Shoot_E",
+               "Ready", "Arm1", "Stand", "Arm2", "Installation","Carry_out"
+            };
+
+        public static class Bit_X
+        {
+            public const int IN_CONVARE_FIRST = 0b0000000000010000;
+            public const int PT_POSITION = 0b0000000000100000;
+            public const int CONVARE_END = 0b0000000001000000;
+            public const int OUT_CONVARE_FIRST = 0b0000000010000000;
+            public const int OUT_CONVARE_END = 0b0000001000000000;
+            public const int STAND_POS = 0b0000010000000000;
+
+
+            public static readonly int[] bits = {IN_CONVARE_FIRST, PT_POSITION, CONVARE_END,
+                                                   OUT_CONVARE_FIRST, OUT_CONVARE_END, STAND_POS };
+
+
+            public static bool getProgressNum(int bits,bool on,ref int progressNum)
+            {
+                switch (bits)
+                {
+                    case IN_CONVARE_FIRST: if (on) { progressNum = 0; return true; } return false;
+                    case PT_POSITION: if (on) { progressNum = 1; return true; } return false;
+                    case CONVARE_END: if (on) progressNum = 4; else progressNum = 5; return true;
+                    case STAND_POS: if (on) progressNum = 6; else progressNum = 7; return true;
+                    case OUT_CONVARE_FIRST: if (on) { progressNum = 8; return true; } return false;
+                    case OUT_CONVARE_END: if (on) { progressNum = 9; return true; } return false;
+                    default: return false;
+                }
+            }
+
+            public static string GetColumName(short bit)
+            {
+                int index = Array.IndexOf(bits, bit);
+
+                if(index < 0)
+                {
+                    return null;
+                }
+                switch (index)
+                {
+                    case 0:
+                        return Parameters.TIME_COLUMNAMES[1];
+                        default : return null;
+
+                }
+            }
+
+            /// <summary>
+            ///  変化を読み取るビットを限定させるマスク。
+            ///  ブロックのうちこれで指定されたビットが変化したら反応する
+            /// </summary>
+            public static short MASK
+            {
+                get
+                {
+                    return
+                    IN_CONVARE_FIRST | PT_POSITION | CONVARE_END | OUT_CONVARE_FIRST |
+                    OUT_CONVARE_END | STAND_POS;
+                }
+            }
+
+        }
+
+        public static class Bits_M
+        {
+            public const int RFID_READ = 0b0001000000000000;
+        }
+    }
+}
