ce567d1feee167380c70c79ad8e887b13d4e6480
Kodai-study <anchor.kou@softbank.ne.jp>
2023-02-19 15:47:32 +0900

    データブロッククラスの作成
    データをブロックで取得したときの、データの保持を担当するモデルクラスの作成

---
+++ PLC_Connection/Modules/DataBlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLC_Connection.Modules
+{
+    public abstract class DataBlock
+    {
+        private int data;
+
+        private int oldData;
+
+        public int BitMask { get; set; } = -1;
+
+        public bool IsNeedInsertData
+        {
+            get { return false; }
+        }
+
+        public List<ChangeBitData> ChangedDatas() {
+            List<ChangeBitData> changeBitDataList = new List<ChangeBitData>();
+
+            return changeBitDataList;
+        }
+
+        public class ChangeBitData
+        {
+            int bitNumber { get; set; }
+            bool isStundUp { get; set; }
+            public ChangeBitData(int bitNumber, bool isStundUp) {
+                this.bitNumber = bitNumber;
+                this.isStundUp = isStundUp;
+            }
+        }
+    }
+}
---
+++ PLC_Connection/Modules/PLCContactData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLC_Connection.Modules
+{
+    public class PLCContactData
+    {
+        int block_X00;
+        int block_Y00;
+    }
+}
